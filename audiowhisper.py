c6fe0980cb9e5bbeb98b6b64d1331be849a730db
drivepc <driveasano@gmail.com>
2024-02-16 12:05:04 +0900

    要約

--- audiowhisper.py
+++ audiowhisper.py
@@ -5,6 +5,16 @@
 import whisper
 import torch
 import json
+import re
+
+# 自動要約
+from pysummarization.nlpbase.auto_abstractor import AutoAbstractor
+from pysummarization.tokenizabledoc.mecab_tokenizer import MeCabTokenizer
+from pysummarization.abstractabledoc.top_n_rank_abstractor import TopNRankAbstractor
+from pysummarization.nlp_base import NlpBase
+from pysummarization.similarityfilter.tfidf_cosine import TfIdfCosine
+from pysummarization.tokenizabledoc.simple_tokenizer import SimpleTokenizer
+import MeCab
 
 
 def secondtotime(time):
@@ -17,11 +27,11 @@ def secondtotime(time):
     sh = str(h).zfill(2)
     sm = str(m).zfill(2)
     ss = str(int(s)).zfill(2)
-    return f'{sh}:{sm}:{ss}.{mm}'
+    return f"{sh}:{sm}:{ss}.{mm}"
 
 
-def wis(mp3file):
-    model = whisper.load_model("large", device="cpu")  # large-v2, medium
+def wis(filename):
+    model = whisper.load_model("large-v2", device="cpu")  # large, medium
     _ = model.half()
     _ = model.cuda()
 
@@ -29,29 +39,137 @@ def wis(mp3file):
         if isinstance(m, whisper.model.LayerNorm):
             m.float()
 
-    print(mp3file)
+    print(filename)
     with torch.no_grad():
         result = model.transcribe(
-            f"{mp3file}",
+            f"{filename}",
             verbose=True,
-            language='japanese',
+            language="japanese",
             # beam_size=5,
             # fp16=True,
             # without_timestamps=False
         )
 
-    f = open(f'{mp3file}.srt', 'a', encoding='UTF-8')
-    count = 0
-    f.write("WEBVTT\n\n")
-    for s in result["segments"]:
-        count += 1
-        f.write(str(count) + '\n' +
-                secondtotime(s["start"]) + " --> " +
-                secondtotime(s["end"]) + "\n" +
-                json.dumps(s["text"], ensure_ascii=False).replace("\"", "") + "\n\n")
+    with open(f"{filename}.json", "w", encoding="UTF-8") as f:
+        json.dump(result, f, indent=4, ensure_ascii=False)
 
-    f.close()
+    with open(f"{filename}.time.txt", "w", encoding="UTF-8") as f:
+        count = 0
+        temp = -1
+        for s in result["segments"]:
+            count += 1
+            if temp == re.sub(r"\..$", "", secondtotime(s["start"])):
+                f.write("\n")
+            else:
+                f.write(
+                    ("" if count == 1 else "\n\n")
+                    + re.sub(r"\..$", "", secondtotime(s["start"]))
+                    + " "
+                    + str(count)
+                    + "\n"
+                )
+
+            temp = re.sub(r"\..$", "", secondtotime(s["end"]))
+            f.write(json.dumps(s["text"], ensure_ascii=False).replace('"', ""))
+
+    with open(f"{filename}.srt", "w", encoding="UTF-8") as f:
+        count = 0
+        for s in result["segments"]:
+            count += 1
+            f.write(
+                str(count)
+                + "\n"
+                + secondtotime(s["start"])
+                + " --> "
+                + secondtotime(s["end"])
+                + "\n"
+                + json.dumps(s["text"], ensure_ascii=False).replace('"', "")
+                + "\n\n"
+            )
+
+    with open(f"{filename}.txt", "w", encoding="UTF-8") as f:
+        count = 0
+        temp = -1
+        document = ""
+        for s in result["segments"]:
+            document += (
+                json.dumps(s["text"], ensure_ascii=False).replace('"', "") + "。"
+            )
+        f.write(document)
+        summarization(filename, document)
 
 
+def summarization(filename, document):
+    print("summarization")
+    similarity_limit = 0.65
+    # 自動要約のオブジェクト
+    auto_abstractor = AutoAbstractor()
+    # トークナイザー設定（MeCab使用）
+    auto_abstractor.tokenizable_doc = MeCabTokenizer()
+    # 区切り文字設定
+    auto_abstractor.delimiter_list = ["。", "\n"]
+    # 抽象化&フィルタリングオブジェクト
+    abstractable_doc = TopNRankAbstractor()
+    # 文書要約
+    result_dict1 = auto_abstractor.summarize(document, abstractable_doc)
 
-wis("./信号理論基礎18.mkv")
+    print(f"summarize_result")
+    with open(f"{filename}.summarize.txt", "w", encoding="UTF-8") as f:
+        # f.write("[要約結果]")
+        for sentence in result_dict1["summarize_result"]:
+            f.write(sentence)
+        f.write("\n\n")
+        f.write(json.dumps(result_dict1, ensure_ascii=False))
+
+    # NLPオブジェクト
+    nlp_base = NlpBase()
+    # トークナイザー設定（MeCab使用）
+    nlp_base.tokenizable_doc = MeCabTokenizer()
+    # 類似性フィルター
+    similarity_filter = TfIdfCosine()
+    # NLPオブジェクト設定
+    similarity_filter.nlp_base = nlp_base
+    # 類似性limit：limit超える文は切り捨て
+    similarity_filter.similarity_limit = similarity_limit
+
+    # 自動要約のオブジェクト
+    auto_abstractor = AutoAbstractor()
+    # トークナイザー設定（MeCab使用）
+    auto_abstractor.tokenizable_doc = MeCabTokenizer()
+    # 区切り文字設定
+    auto_abstractor.delimiter_list = ["。", "\n"]
+    # 抽象化&フィルタリングオブジェクト
+    abstractable_doc = TopNRankAbstractor()
+    # 文書要約（similarity_filter機能追加）
+    result_dict2 = auto_abstractor.summarize(
+        document, abstractable_doc, similarity_filter
+    )
+
+    print("summarize_result2")
+    with open(f"{filename}.summarize_sf.txt", "w", encoding="UTF-8") as f:
+        # f.write("[要約結果：Similarity Filter機能]")
+        for sentence in result_dict2["summarize_result"]:
+            f.write(sentence)
+        f.write("\n\n")
+        f.write(json.dumps(result_dict2, ensure_ascii=False))
+
+
+def main(files):
+    for file in files:
+        wis(file)
+
+
+def summarize(files):
+    for file in files:
+        with open(f"{file}", encoding="UTF-8") as f:
+            summarization(file, f.readline().replace(" ", ""))
+
+
+if __name__ == "__main__":
+    files = [
+        "./日本人の教養-4-前半.mp4",
+        # "./日本人の教養-4-後半-1.mp4",
+        # "./日本人の教養-4-後半-2.mp4",
+    ]
+    main(files)
+    # summarize(files)
